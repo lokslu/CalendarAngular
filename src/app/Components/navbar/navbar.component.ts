d6e25950e2ed18aa6c90854e02bb13e3daa87759
Александр Мельник <lokslu6@gmail.com>
2021-08-31 02:18:54 +0200

    Adds navbar functionality

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+  @Input() Now: Date;
+  @Output() change = new EventEmitter();
+  public CurDate: string;
 
   ngOnInit(): void {
-  }
+    this.CurDate = this.NowDate();
 
+  }
+  public ResetNow()
+  {
+    this.Now.setTime(new Date().getTime());
+    this.change.emit()
+    this.CurDate = this.NowDate();
+    console.log(this.Now)
+
+
+  }
+  public SetLeftMonth() {
+    this.Now.setMonth(this.Now.getMonth() - 1);
+    this.change.emit()
+    this.CurDate = this.NowDate();
+
+    console.log(this.Now)
+
+  }
+  public SetRightMonth() {
+    this.Now.setMonth(this.Now.getMonth() + 1);
+    this.change.emit()
+    this.CurDate = this.NowDate();
+
+    console.log(this.Now)
+
+  }
+  public NowDate() {
+    return this.Now.toLocaleDateString('en', { year: 'numeric', month: 'long' })
+  }
 }
